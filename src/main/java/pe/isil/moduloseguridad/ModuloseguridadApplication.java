65b87c45e798e487ec78f520e1ec2c19a2d37f6e
Hellscythe25 <46532421+Hellscythe25@users.noreply.github.com>
2022-09-26 16:44:33 -0500

    Ep1

--- src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
+++ src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
@@ -3,12 +3,71 @@ package pe.isil.moduloseguridad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
 @SpringBootApplication
 public class ModuloseguridadApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(ModuloseguridadApplication.class, args);
-		System.out.print("Initializing...");
+		System.out.println("Initializing...");
+
+		//Load Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		//Create Connexion
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moduloseg2", "root", "root1234");
+
+		//testStatement(connection);
+		//testPreparedStatement(connection);
+		//testPreparedStatementResult(connection);
+		testCallableStatement(connection);
+
+		connection.close();
+	}
+
+	public static void testStatement(Connection connection) throws Exception{
+		//Create statement
+		Statement statement = connection.createStatement();
+
+		//Execute sentence
+		int affectedRows = statement.executeUpdate("UPDATE USERS SET name='Jose' WHERE id=1");
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+		while (resultSet.next()){
+			System.out.println(resultSet.getString("name"));
+		}
+		System.out.println("Affected Rows: "+affectedRows);
+	}
+
+	//ACTUALIZAR REGISTRO
+	public static void testPreparedStatement(Connection connection) throws Exception{
+		PreparedStatement preparedStatement = connection.prepareStatement("Update USERS SET name=? WHERE id=?");
+		preparedStatement.setString(1, "Eduardo");
+		preparedStatement.setInt(2,3);
+
+		int affectedRows = preparedStatement.executeUpdate();
+		System.out.println("Affected Rows: "+affectedRows);
+
+	}
+	public static void testPreparedStatementResult(Connection connection) throws Exception{
+		PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM USERS WHERE username=?");
+		preparedStatement.setString(1, "RastaKing");
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()){
+			System.out.println(resultSet.getString("name")+" "+resultSet.getString("lastname"));
+		}
+
+	}
+
+	// LISTAR CON PROCEDIMIENTOS ALMACENADOS
+	public static void testCallableStatement(Connection connection) throws Exception{
+		CallableStatement callableStatement = connection.prepareCall("{call getAllUsers()}");
+		ResultSet resultSet = callableStatement.executeQuery();
+		while (resultSet.next()){
+			System.out.println(resultSet.getString("name")+" "+resultSet.getString("lastname"));
+		}
 
 	}
 
